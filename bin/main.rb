c18819c86a5df91dbe2dda7b32a56bcdbdc456f9
Taiwo Coker <51359447+taiwocoker@users.noreply.github.com>
2020-05-20 10:44:53 +0100

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -8,7 +8,21 @@ board = 1
 while board <= valid_options.size
   puts "Player 1, it is your turn"
   puts "player 1,select between number 1 and 9"
+  #prompt user for a selection
   choice_selected = gets.chomp
+  #check if the choice selected is valid and the position is not taken
+  until valid_options.include?(choice_selected) && !choice_options.include?(choice_selected)
+    puts "please put a number between 1 and 9 and make sure it has not been selected"
+    choice_selected = gets.chomp
+  end
 
-  until valid_options.include?(choice_selected) && 
+  #update the board
+  puts "The board has now been updated"
+  choice_options.push(choice_selected)
+  puts "The board is displayed"
+  
+  return puts "player 1 has won the game" if game_won
+  return puts "The game has ended in draw" if board == valid_options.size
+
+  board += 1
 end
