420ab0c72a1d9f504b0401134ffafce8da481519
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-20 12:49:07 +0300

    add content for player 2

--- bin/main.rb
+++ bin/main.rb
@@ -25,4 +25,24 @@ while board <= valid_options.size
   return puts "The game has ended in draw" if board == valid_options.size
 
   board += 1
+
+  puts "Player 2, it is your turn"
+  puts "player 2,select between number 1 and 9"
+  #prompt user for a selection
+  choice_selected = gets.chomp
+  #check if the choice selected is valid and the position is not taken
+  until valid_options.include?(choice_selected) && !choice_options.include?(choice_selected)
+    puts "please put a number between 1 and 9 and make sure it has not been selected"
+    choice_selected = gets.chomp
+  end
+
+  #update the board
+  puts "The board has now been updated"
+  choice_options.push(choice_selected)
+  puts "The board is displayed"
+  
+  return puts "player 2 has won the game" if game_won
+  return puts "The game has ended in draw" if board == valid_options.size
+
+  board += 1
 end
