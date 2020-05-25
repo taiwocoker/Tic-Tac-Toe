3c06bdb1c3f5ceed5e08327f21a55fa40b3ad088
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-25 12:11:47 +0300

    create method to define player's move
    take player name and validate the option picked on the bord. Until its valid option update the board

--- bin/main.rb
+++ bin/main.rb
@@ -5,3 +5,19 @@ require_relative '../lib/validate'
 
 tictactoe = Board.new
 validation = Validate.new
+
+def player_move(player_name, validation, board)
+  puts "player #{player_name} turn!"
+  puts "player #{player_name} select a number from 1 to 9"
+  option_selected = gets.chomp
+
+  until validation.valid_input(option_selected)
+    puts 'please put a number between 1 to 9 and make sure it has not been selected'
+    option_selected = gets.chomp
+  end
+
+  validation.update_selected_position(option_selected)
+
+  board.update_board(option_selected, player_name)
+  board.display_board
+end
