47c41c40e0401d285424e517223c977d320b3954
Taiwo Coker <taiwofcoker@gmail.com>
2020-05-25 09:34:11 +0100

    add methods for player won and full board

--- lib/board.rb
+++ lib/board.rb
@@ -35,7 +35,47 @@ class Board
       board[index] = character
       return board
    end
+   
+   def position_taken?(board, index)
+      if board[index] == " " || board[index] == "" || board[index] == nil
+        return false
+      else
+        return true
+      end
+   end
+
+   def position_taken?(board, index)
+      if board[index] == " " || board[index] == "" || board[index] == nil
+        return false
+      else
+        return true
+      end
+    end
+    def won?(board)
+      win_criteria.each {|win_combo|
+        index_0 = win_combo[0]
+        index_1 = win_combo[1]
+        index_2 = win_combo[2]
     
+        position_1 = board[index_0]
+        position_2 = board[index_1]
+        position_3 = board[index_2]
+    
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return win_combo
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combo
+        end
+      }
+      return false
+    end
+    
+    def full?(board)
+      board.all? {|index| index == "X" || index == "O"}
+    end
+     
+    
+
 
    
 end
