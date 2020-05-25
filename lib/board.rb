d628a783f9ca6bddae6d9474980fa637824823d4
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-25 11:05:28 +0300

    add puts to display board

--- lib/board.rb
+++ lib/board.rb
@@ -13,7 +13,14 @@ class Board
         [6, 4, 2]
       ]
    end
-   def display_board
-       
+   def display_board(board)
+    separator = "|"
+    lines = "-----------"
+    
+    puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+    puts "#{lines}"
+    puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+    puts "#{lines}"
+    puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
    end
 end
