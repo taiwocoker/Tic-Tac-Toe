6832bf7b7c0989f9d1440288130f39a4b98dda91
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-25 11:00:42 +0300

    add win criteria on the board

--- lib/board.rb
+++ lib/board.rb
@@ -2,5 +2,18 @@ class Board
    attr_reader :board
    def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
+    @win_criteria = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+      ]
+   end
+   def display_board
+       
    end
 end
