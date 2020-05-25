39683130b39f1b15d7422e09a8c2e4c9a89aaf7a
Hillary Kiptoo <hillaryodhiambo282@gmail.com>
2020-05-25 12:00:51 +0300

    require board and validate
    create instance of board and create new c=validation

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
-# !usr/bin/env/ ruby
+# !/usr/bin/env ruby
 
 require_relative '../lib/board'
 require_relative '../lib/validate'
 
-board.display_board
+tictactoe = Board.new
+validation = Validate.new
