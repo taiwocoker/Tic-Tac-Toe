6528a91ffc83e46e93bab63f128f8d90b2dcbde0
Taiwo Coker <taiwofcoker@gmail.com>
2020-05-31 20:27:10 +0100

    fix the git flow and fix the linter issues

---
+++ spec/validate_spec.rb
@@ -0,0 +1,26 @@
+require './lib/validate'
+require './lib/board'
+
+describe Validate do
+  let(:validation) { Validate.new }
+  let(:board) { Board.new }
+
+  describe 'valid_input' do
+    it 'should return true if a valid option is inputted' do
+      expect(validation.valid_input('3')).to eq(true)
+    end
+
+    it 'should return false if an alphabet is inputted' do
+      expect(validation.valid_input('A')).to eq(false)
+    end
+
+    it 'should return false if the number is out of range' do
+      expect(validation.valid_input('57')).to eq(false)
+    end
+
+    it 'should return false the position selected has been selected earlier' do
+      validation.update_selected_position('3')
+      expect(validation.valid_input('3')).to eq(false)
+    end
+  end
+end
