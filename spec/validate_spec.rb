110804e42b385e1d46d16237ec43157bd837cbfc
imhilla <hillaryodhiambo282@gmail.com>
2020-06-09 08:13:32 +0300

    add test for validation to return true or false

--- spec/validate_spec.rb
+++ spec/validate_spec.rb
@@ -3,4 +3,11 @@ require_relative '../lib/board'
 describe '#Validate' do
   let(:validation) { Validate.new }
   let(:board) { Board.new }
+  describe '#valid_input' do
+    it 'returns true if position is within valid options' do
+        expect(validation.valid_input('5')).to eql(true)
+    end
+    it 'returns false if position is not within options' do 
+    end
+  end
 end
