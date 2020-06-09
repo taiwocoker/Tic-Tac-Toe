4fda3bb13bdb2368462970f3e89c246052fc28c5
imhilla <hillaryodhiambo282@gmail.com>
2020-06-09 07:39:27 +0300

    add tests for update boad

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,14 @@
 require_relative '../lib/board'
 
 describe Board do
+  let(:tictactoe) { Board.new }
+  describe '#display_board' do
+    it 'should create and display a board' do
+      board = tictactoe.board
+      expect(board[0]).to eql(1)
+      expect(board[13]).to eql(nil)
+    end
+  end
+  describe '#update_board' do
+  end
 end
